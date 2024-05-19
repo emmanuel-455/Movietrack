548eb8d177747b7efa3fa96871fe112ee682e817
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-05-19 15:22:26 +0100

    update

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -20,7 +20,7 @@ const MovieList: React.FC<MovieListProps> = ({ movies }) => {
   return (
     <>
       <Header movies={movies} />
-      <div className='p-3 md:px-[150px] py-16'>
+      <div className='p-3 sm:px-full md:px-[150px] py-[50px]'>
         <h1 className='text-xl text-[white] font-semibold mb-5'>Popular Movies</h1>
       <div className="movies-container m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 ">
       {movies.map((movie) => (
@@ -40,7 +40,8 @@ const MovieList: React.FC<MovieListProps> = ({ movies }) => {
               <h2 className="text-[17px] md:text-xl font-bold mb-[2px] text-[#e2e2e2]">{movie.title}</h2>
               <p className="text-xs font-semibold italic text-[#9c9c9c] mb-1">Release Date: {movie.release_date}</p>
               <p title={movie.overview} className="text-[15px] line-clamp-2 text-[#d4d4d4] font-semibold">{movie.overview}</p>
-              <p className="text-xs mb-4 text-[#fdfdfd] mt-2">Popularity: {movie.popularity}</p>
+              <p className="text-xs text-[#fdfdfd] mb-2">Popularity: {movie.popularity}</p>
+              <p className='text-right font-bold text-white mr-2 mb-2 text-lg'>View Details</p>
             </div>
           </div>
         </Link>
